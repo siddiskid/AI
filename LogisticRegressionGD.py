d23e48e4d89a7f6ecfb8696de14b1a92d4260123
Siddarth <siddarth_ch@outlook.com>
2024-07-30 04:28:46 -0700

    added files

---
+++ LogisticRegressionGD.py
@@ -0,0 +1,73 @@
+import numpy as np
+
+class LogisticRegressionGD:
+    """
+    Logistic Regression Classifier that learns using Full Batch Gradient Descent
+
+    Params:
+    eta: float
+        Learning rate (between 0.0 and 1.0)
+    n_iter: int
+        Number of passes over the training set (epochs)
+    random_state: int
+        Seed variable for random weight initialization
+    
+    Attributes:
+    w_: 1-d array
+        weights of the perceptron after fitting
+    b_: scalar
+        bias of the perceptron after fitting
+    losses_: list
+        losses in each epoch during fitting
+
+    """
+
+    def __init__(self, eta = 0.01, n_iter = 50, random_state = 1):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.random_state = random_state
+
+    def fit(self, X, y):
+        """
+        Params:
+        X: {array-like} (matrix), shape = [n_examples, n_features]
+            A matrix consisting of training vectors. n_examples
+            is the number of examples and n_features is the number
+            of features.
+        y: array-like, shape = [n_examples]
+            Target values
+
+        Returns:
+        self
+        """
+        self.initialize_weights(X.shape[1])
+        self.losses_ = []
+
+        for _ in range(self.n_iter):
+            net_i = self.net_input(X)
+            out = self.activation(net_i)
+            errors = y - out
+            self.w_ += 2 * self.eta * X.T.dot(errors) / X.shape[0]
+            self.b_ += 2 * self.eta * errors.mean()
+            self.losses_.append((-y.dot(np.log(out)) - ((1 - y).dot(1 - np.log(out)))) / X.shape[0])
+
+        return self
+    
+    def initialize_weights(self, size):
+        """Initialize weights and bias to small, random numbers"""
+        rgen = np.random.RandomState(self.random_state)
+        self.w_ = rgen.normal(0.0, 0.01, size)
+        self.b_ = 0.0
+        
+    def net_input(self, X):
+        """Returns an array of the net inputs for each input in X (weights * inputs + bias)"""
+        return np.dot(X, self.w_) + self.b_
+    
+    def activation(self, z):
+        """Returns result of passing net input through an activation function (here, sigmoid)"""
+        return 1 / (1 + np.exp(-np.clip(z, -250, 250)))
+    
+    def predict(self, X):
+        """Return an array of predicted classes"""
+        return np.where(self.activation(self.net_input(X)) >= 0.5, 1, 0)
+
